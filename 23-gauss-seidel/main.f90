43f7cadd3c2aa3f8ff97c034cdce6d810740ff6e
Antu Saha <imantu09@gmail.com>
2025-03-28 13:07:55 +0600

    add: gauss-seidel

---
+++ 23-gauss-seidel/main.f90
@@ -0,0 +1,47 @@
+! Solve the follow system of equation with Gauss-Seidel method. Use (0, 0, 0, 0) as initial guess and tolerance of 0.001
+! 10w - x + 2y = 6
+! - w + 11x - y + 3z = 25
+! 2w - x + 10y - z = -11
+! 3x - y + 8z = 15
+
+program GaussSeidel
+  implicit none
+
+  real, allocatable :: A(:, :), b(:), xo(:), x(:)
+  integer :: i, j, k, n, max
+  real :: tol
+
+  write (*, *) "Enter the number of equations, max iteration, tolerance:"
+  read (*, *) n, max, tol
+
+  allocate(A(n,n), b(n), xo(n), x(n)) ! Allocate necessary memory
+
+  write (*, *) "Enter the coefficient matrix A:"
+  read (*, *) ((A(i, j), j = 1, n), i = 1, n)
+  write (*, *) "Enter the constant matrix b:"
+  read (*, *) (b(i), i = 1, n)
+  write (*, *) "Enter initial guess:"
+  read (*, *) (x(i), i = 1, n)
+
+  write(*, *) ! Adds a new line
+  write (*, *) "Iteration  table"
+  write (*, *) repeat("-", 100)
+
+  k = 1
+  do while (k <= max)
+    do i = 1, n
+      x(i) = (b(i) - sum(A(i, 1:i-1) * x(1:i-1)) - sum(A(i, i+1:n) * xo(i+1:n))) / A(i, i)
+    end do
+
+    write (*, *) k, x
+    if (norm2(x - xo) < tol) then
+      write (*, *) repeat("-", 100)
+      write (*, *) "Approximate solution: ", x
+      stop
+    end if
+    k = k + 1
+    xo = x
+  end do
+  write (*, *) repeat("-", 100)
+  write (*, *) "Maximum number of iterations exceeded"
+end program GaussSeidel
--- README.md
+++ README.md
@@ -39,6 +39,7 @@ This repository contains a collection of numerical methods implemented in modern
 - [Gaussian Elimination with Backward Substitution](./20-gaussian-elimination-with-backward-substitution/main.f90)
 - [Gaussian Elimination with LU Factorization](./21-gaussian-elimination-with-lu-factorization/main.f90)
 - [Jacobi Method](./22-jacobi/main.f90)
+- [Gauss-Seidel Method](./23-gauss-seidel/main.f90)
 
 ### 6. Initial Value Problem for ODE
 
